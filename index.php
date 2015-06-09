2e2137635624d3cf605b0c7d714a5fb8bfb34fc5
bnqqnb <bnqqnb@hotmail.com>
2015-06-09 19:23:07 +0800

    FUNCTION2

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
-    echo 'CICICICIAAAAA';
-    echo 'DIDIDID';
-    echo 'DIDIDID222';
-    
-    echo 'bnqqnb1';
-    echo 'bnqqnb2';
-    echo 'bnqqnb3';
-    echo 'bnqqnb4';
-    
-    
+    echo 'MAIN START';
     
     echo 'MAIN FUNCTION';
     echo 'GREAT';
     
-    
     echo 'MY FUNCTION2';
     echo 'VERY GREAT FUNCTION';
-    
+
+    echo 'VERY GOOD';
 ?>
